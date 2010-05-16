6ab31b479d018e8985075cacf8d81dadc50e73b2
Corey <coyotebush22@gmail.com>
2010-05-16 11:37:11 -0700

    tests and validations for Activity

--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -5,10 +5,19 @@ class ActivityTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
-  #test "should not be valid" do
-  #  @activity = Activity.new
-  #  assert !@activity.valid?
+  test "should not be valid" do
+    @activity = Activity.new
+    assert !@activity.valid?
 	
-  # @activity = Activity.new :
+    @activity = Activity.new :start_time => "2010-05-16 10:08:46"
+    assert !@activity.valid?
+	
+    @activity = Activity.new :end_time => "2010-05-16 10:08:46"
+    assert !@activity.valid?
+	
+	# Must start before it ends
+	@activity = Activity.new :start_time => "2010-05-16 10:08:46", :end_time => "2010-05-16 8:08:23"
+    assert !@activity.valid?
+  end
 	
 end
