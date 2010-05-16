92696bae693c3611af81071e7de3b8d34689ceeb
Corey <coyotebush22@gmail.com>
2010-05-16 16:14:00 -0700

    extract custom datetime_select into a helper

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,5 @@
 module ActivitiesHelper
+  def recent_datetime_select (f, field, options = {})
+    f.datetime_select field, {:start_year => Time.now.year - 1, :end_year => Time.now.year}.merge(options)
+  end
 end
