42883ca123c9106bb89ed9583615f3a1c68dc8c8
Corey <coyotebush22@gmail.com>
2010-05-16 18:13:22 -0700

    fixups in tests, add "should be valid"

--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
   test "should not be valid" do
     @activity = Activity.new
     assert !@activity.valid?
-	
+
     @activity = Activity.new :start_time => "2010-05-16 10:08:46"
     assert !@activity.valid?
-	
+
     @activity = Activity.new :end_time => "2010-05-16 10:08:46"
     assert !@activity.valid?
-	
+
 	# Must start before it ends
 	@activity = Activity.new :start_time => "2010-05-16 10:08:46", :end_time => "2010-05-16 8:08:23"
     assert !@activity.valid?
   end
-	
+
+  test "should be valid" do
+    @activity = Activity.new :start_time => "2010-05-16 10:08:46", :end_time => "2010-05-16 10:54:13"
+    assert @activity.valid?
+  end
 end
