09f3ecd11a3229382e209b7e700b23f59712fc2e
Corey <coyotebush22@gmail.com>
2010-05-16 18:15:18 -0700

    format distance to 2 decimal places

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -9,6 +9,10 @@ class Activity < ActiveRecord::Base
 	"#{hours}:#{minutes}"
   end
 
+  def formatted_distance
+    "%.2f" % distance unless distance.nil?
+  end
+
   def must_start_before_ending
     if !end_time.nil? and !start_time.nil? and end_time < start_time
       errors.add_to_base("End time cannot be before start time")
