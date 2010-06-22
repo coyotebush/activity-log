a9382efe06d6b130b47c9b039d4bcfa229faa9c6
Corey <coyotebush22@gmail.com>
2010-06-22 10:46:15 -0700

    better totals

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,6 +13,8 @@ class ReportsController < ApplicationController
         :conditions => ['start_time BETWEEN ? AND ?', @start_date, @end_date],
         :order => 'start_time')
     end
+    @total_time     = sum @activities.map(&:elapsed)
+    @total_distance = sum @activities.map(&:distance)
   end
 
   private
@@ -20,5 +22,9 @@ class ReportsController < ApplicationController
       unless p.nil? or p[:year].nil? or p[:month].nil? or p[:day].nil?
         Date.civil(p[:year].to_i, p[:month].to_i, p[:day].to_i)
       end
+    end
+
+    def sum xs
+      xs.find_all {|x| x.is_a? Numeric }.sum
     end
 end
