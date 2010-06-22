7b7400c66f071e4512b9af4cc2bad53cd3881200
Corey <coyotebush22@gmail.com>
2010-06-21 21:31:28 -0700

    a simple report, differentiated from listing view

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,8 +3,15 @@ class ReportsController < ApplicationController
   end
 
   def report
-    @start_date = params(:start)
-    @end_date = params(:end)
-    
+    @start_date = parse_select_date(params[:first])
+    @end_date = parse_select_date(params[:last])
+    @activities = Activity.find(:all,
+      :conditions => ['start_time BETWEEN ? AND ?', @start_date, @end_date],
+      :order => 'start_time')
   end
+
+  private
+    def parse_select_date(p)
+      Date.civil(p[:year].to_i, p[:month].to_i, p[:day].to_i)
+    end
 end
