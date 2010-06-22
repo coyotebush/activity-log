41fc86d18baa657d77a5fc6c8bc868a63f7ac5f5
Corey <coyotebush22@gmail.com>
2010-06-22 10:46:05 -0700

    cleaner parameter handling

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,11 +3,12 @@ class ReportsController < ApplicationController
   end
 
   def report
-    if params[:first].nil? or params[:last].nil?
+    @start_date = parse_select_date(params[:first])
+    @end_date = parse_select_date(params[:last])
+
+    if @start_date.nil? or @end_date.nil?
       @activities = Activity.find(:all, :order => 'start_time')
     else
-      @start_date = parse_select_date(params[:first])
-      @end_date = parse_select_date(params[:last])
       @activities = Activity.find(:all,
         :conditions => ['start_time BETWEEN ? AND ?', @start_date, @end_date],
         :order => 'start_time')
@@ -16,6 +17,8 @@ class ReportsController < ApplicationController
 
   private
     def parse_select_date(p)
-      Date.civil(p[:year].to_i, p[:month].to_i, p[:day].to_i)
+      unless p.nil? or p[:year].nil? or p[:month].nil? or p[:day].nil?
+        Date.civil(p[:year].to_i, p[:month].to_i, p[:day].to_i)
+      end
     end
 end
