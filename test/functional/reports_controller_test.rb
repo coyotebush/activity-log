551ba2d415d7c6f73625bdcff76418c46d1305a2
Corey <coyotebush22@gmail.com>
2010-06-25 11:33:33 -0700

    simple tests for report action

--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -5,4 +5,13 @@ class ReportsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
   end
+
+  test "should get report" do
+    get :report
+    assert_response :success
+
+    get :report, :start_date => { :year => "2010", :month => "2", :day => "2" },
+                 :end_date => { :year => "2010", :month => "10", :day => "9" }
+    assert_response :success
+  end
 end
